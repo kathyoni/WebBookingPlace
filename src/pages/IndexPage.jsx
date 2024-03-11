abd2e9e005914261378f436a5efc6618d8f86714
Phạm Hoàng Quốc Đạt <phamhoangquocdat@MacBook-Air-cua-Pham.local>
2024-03-11 22:04:44 +0700

    commit 2

---
+++ src/pages/IndexPage.jsx
@@ -0,0 +1,11 @@
+import { Link, Route } from "react-router-dom";
+import Header from "../Header";
+
+
+export default function IndexPage() {
+  return (
+    <div>
+      index page here
+    </div>
+  );
+}
