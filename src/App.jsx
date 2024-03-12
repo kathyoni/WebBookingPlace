169b2f248cb7a91a4b45090dcc842f9859828d7e
Phạm Hoàng Quốc Đạt <phamhoangquocdat@MacBook-Air-cua-Pham.local>
2024-03-12 23:53:02 +0700

    commit 3

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,20 @@ import IndexPage from "./pages/IndexPage.jsx";
 import Layout from "./Layout.jsx";
 import RegisterPage from "./pages/RegisterPage.jsx";
 import axios from "axios";
+import { UserContextProvider } from "./UserContext.jsx";
 axios.defaults.baseURL = "http://localhost:4000";
+axios.defaults.withCredentials = true; 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<IndexPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-      </Route>
-    </Routes>
+    <UserContextProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<IndexPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Route>
+      </Routes>
+    </UserContextProvider>
   );
 }
 
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -5,13 +5,20 @@ export default function RegisterPage() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  function registerUser(ev) {
+  async function registerUser(ev) {
     ev.preventDefault();
-    axios.post('/register',{
-      name,
-      email,
-      password
-    });
+    try {
+      await axios.post("/register", {
+        name,
+        email,
+        password,
+      });
+      alert("User registration successful. Now you can log in");
+    } catch (error) {
+      alert("User registration failed, please try again later!!");
+    }
+    
+    
   }
   return (
     <div className="mt-4 grow flex items-center justify-around">
