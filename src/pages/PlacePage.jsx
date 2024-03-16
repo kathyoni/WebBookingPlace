d0cc9ebc03dc8248b025759d1ff15b893cc1b798
Phạm Hoàng Quốc Đạt <phamhoangquocdat@MacBook-Air-cua-Pham.local>
2024-03-16 23:39:17 +0700

    Client commit 6

--- src/pages/PlacePage.jsx
+++ src/pages/PlacePage.jsx
@@ -1,29 +1,151 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import Perks from "../Perk";
 
 export default function PlacesPage() {
+  const { action } = useParams();
+  const { title, setTitle } = useState("");
+  const { address, setAddress } = useState("");
+  const { addPhotos, setAddPhotos } = useState([]);
+  const { photoLink, setPhotoLink } = useState("");
+  const { description, setDescription } = useState("");
+  const { perks, setPerks } = useState([]);
+  const { extraInfo, setExtraInfo } = useState("");
+  const { checkIn, setCheckIn } = useState("");
+  const { checkOut, setCheckOut } = useState("");
+  const { maxGuests, setMaxGuests } = useState("1");
+
+  function inputHeader(text) {
+    return <h2 className="text-2xl mt-4">{text}</h2>;
+  }
+  function inputDescription(text) {
+    return <p className=" text-gray-500 text-sm">{text}</p>;
+  }
+  function preInput(header, description) {
+    return (
+      <>
+        {inputHeader(header)}
+        {inputDescription(description)}
+      </>
+    );
+  }
   return (
     <div>
-      <div className="text-center">
-        <Link
-          className="inline-flex gap-1 bg-primary text-white px-4 py-2 rounded-full"
-          to={"/account/place/new"}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="w-6 h-6"
+      {action !== "new" && (
+        <div className="text-center">
+          <Link
+            className="inline-flex gap-1 bg-primary text-white px-4 py-2 rounded-full"
+            to={"/account/places/new"}
           >
-            <path
-              fillRule="evenodd"
-              d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z"
-              clipRule="evenodd"
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                fillRule="evenodd"
+                d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z"
+                clipRule="evenodd"
+              />
+            </svg>
+            Add new place
+          </Link>
+        </div>
+      )}
+      {action === "new" && (
+        <div>
+          <form>
+            {preInput("Title", "Title for your place")}
+            <input
+              type="text"
+              value={title}
+              onChange={(ev) => setTitle(ev.target.value)}
+              placeholder="title, for example"
             />
-          </svg>
-          Add new place
-        </Link>
-      </div>
-      place
+            {preInput("Address", "Address for your place")}
+            <input
+              type="text"
+              value={address}
+              onChange={(ev) => setAddress(ev.target.value)}
+              placeholder="adress"
+            />
+            {preInput("Photos", "more = better")}
+            <div className="flex gap-2">
+              <input
+                value={photoLink}
+                onChange={(ev) => setPhotoLink(ev.target.value)}
+                type="text"
+                placeholder="{Add using a link...jpg}"
+              />
+              <button className="bg-gray-200 rounded-2xl px-4">
+                Add&nbsp;Photo
+              </button>
+            </div>
+            <div className="mt-2 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
+              <button className="flex gap-1 justify-center border bg-transparent rounded-2xl p-8 text-2xl text-gray-600 ">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-8 h-8"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 16.5V9.75m0 0 3 3m-3-3-3 3M6.75 19.5a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.23-2.3 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z"
+                  />
+                </svg>
+                Upload
+              </button>
+            </div>
+            {preInput("Description", "depcription of the place")}
+            <textarea
+              value={description}
+              onChange={(ev) => setDescription(ev.target.value)}
+            />
+            {preInput("Perk", "select all the perk of your place")}
+            <div className="mt-2 grid gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
+              <Perks selected={perks} onChange={setPerks} />
+            </div>
+            {preInput("Extra info", "house,rule,etc")}
+            <textarea
+              value={extraInfo}
+              onChange={(ev) => setExtraInfo(ev.target.value)}
+            />
+            {preInput("Check in&out time", "Check in and check out ")}
+            <div className="grid gap-2 sm:grid-cols-3">
+              <div>
+                <h3 className="mt-2 -mb-1">Check in time</h3>
+                <input
+                  type="text"
+                  value={checkIn}
+                  onChange={(ev) => setCheckIn(ev.target.value)}
+                />
+              </div>
+              <div>
+                <h3 className="mt-2 -mb-1">Check out time</h3>
+                <input
+                  type="text"
+                  value={checkOut}
+                  onChange={(ev) => setCheckOut(ev.target.value)}
+                />
+              </div>
+              <div>
+                <h3 className="mt-2 -mb-1">Max number of guests</h3>
+                <input
+                  type="number"
+                  value={maxGuests}
+                  onChange={(ev) => setMaxGuests(ev.target.value)}
+                />
+              </div>
+            </div>
+            <button className="primary my-4">Save</button>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
