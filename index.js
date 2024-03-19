ed50a4131b99c5e7fdc8f57384308b3854b62b0f
Phạm Hoàng Quốc Đạt <phamhoangquocdat@MacBook-Air-cua-Pham.local>
2024-03-19 13:03:55 +0700

    API commit 5

--- index.js
+++ index.js
@@ -7,11 +7,15 @@ const User = require("./models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const cookeParser = require("cookie-parser");
+const imageDownloader = require("image-downloader");
 const bcryptSalt = bcrypt.genSaltSync(10);
 const jwtSecret = "djnlawnflasiscazij";
+const multer = require("multer");
+const fs = require("fs");
 
 app.use(express.json());
 app.use(cookeParser());
+app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
   cors({
     credentials: true,
@@ -45,7 +49,7 @@ app.post("/login", async (req, res) => {
     const passOK = bcrypt.compareSync(password, userDoc.password);
     if (passOK) {
       jwt.sign(
-        { email: userDoc.email, id: userDoc._id},
+        { email: userDoc.email, id: userDoc._id },
         jwtSecret,
         {},
         (err, token) => {
@@ -66,16 +70,37 @@ app.get("/profile", async (req, res) => {
   if (token) {
     jwt.verify(token, jwtSecret, {}, async (err, userData) => {
       if (err) throw err;
-      const {name,email,id} = await User.findById(userData.id);
-      res.json({name,email,id});
+      const { name, email, id } = await User.findById(userData.id);
+      res.json({ name, email, id });
     });
   } else {
     res.json(null);
   }
 });
+app.post("/logout", (req, res) => {
+  res.cookie("token", "").json(true);
+});
+app.post("/upload-by-link", async (req, res) => {
+  const { link } = req.body;
+  const newName = "photo" + Date.now() + ".jpg";
+  await imageDownloader.image({
+    url: link,
+    dest: __dirname + "/uploads/" + newName,
+  });
+  res.json(newName);
+});
 
-app.post("/logout",  (req, res) => {
-  res.cookie("token", '').json(true)
-})
-
+const photosMiddleware = multer({ dest: "uploads" });
+app.post("/upload", photosMiddleware.array("photos", 100), async (req, res) => {
+  const uploadedFiles = []; 
+  for (let i = 0; i < req.files.length; i++) {
+    const { path, originalname } = req.files[i];
+    const parts = originalname.split(".");
+    const ext = parts[parts.length - 1];
+    const newPath = path + '.' + ext;
+    fs.renameSync(path, newPath);
+    uploadedFiles.push(newPath.replace('uploads/',''))
+  }
+  res.json(uploadedFiles);
+});
 app.listen(4000);
